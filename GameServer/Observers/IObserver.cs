56f48905568464a77f93fe790cc16d33e57eb2b1
Aidas <linikas.aidas@gmail.com>
2019-10-05 18:57:05 +0300

    Add Observer barebones

---
+++ GameServer/Observers/IObserver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Observers
+{
+    interface Observer
+    {
+        public void update(int Health);
+    }
+}
