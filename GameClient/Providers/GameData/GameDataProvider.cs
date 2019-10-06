ca15bd871c04574c1b1e74a963bfd7bf96c69ce5
Emilis Makutėnas <emilis.em4@gmail.com>
2019-10-06 14:22:49 +0300

    Add observer pattern with GameData

---
+++ GameClient/Providers/GameData/GameDataProvider.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameClient.Providers.GameData
+{
+    public class GameDataProvider : IObservable<GameData>
+    {
+        private readonly List<IObserver<GameData>> _observers;
+
+        private readonly GameData _gameData;
+        public GameDataProvider()
+        {
+            _observers = new List<IObserver<GameData>>();
+
+            _gameData = new GameData
+            {
+                Health = 100
+            };
+        }
+
+        public IDisposable Subscribe(IObserver<GameData> observer)
+        {
+            if (!_observers.Contains(observer))
+            {
+                _observers.Add(observer);
+            }
+
+            return new GameDataUnsubscriber(_observers, observer);
+        }
+
+        public void SendMessage()
+        {
+            bool start = true;
+
+            while (start || _gameData.Health > 0)
+            {
+                foreach (IObserver<GameData> observer in _observers)
+                {
+                    observer.OnNext(_gameData);
+                }
+
+                if (start)
+                {
+                    start = false;
+                }
+
+                _gameData.Health -= 5;
+            }
+
+            foreach (var observer in _observers)
+            {
+                observer?.OnCompleted();
+            }
+
+            _observers.Clear();
+        }
+    }
+}
---
+++ GameClient/Providers/GameData/GameDataUnsubscriber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameClient.Providers.GameData
+{
+    public class GameDataUnsubscriber : IDisposable
+    {
+        private readonly List<IObserver<GameData>> _observers;
+        private readonly IObserver<GameData> _observer;
+
+        public GameDataUnsubscriber(List<IObserver<GameData>> observers, IObserver<GameData> observer)
+        {
+            _observer = observer;
+            _observers = observers;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null)
+            {
+                _observers.Remove(_observer);
+            }
+        }
+    }
+}
