d7a06b04f7b1cbf569c60715f4c78e1887c7f826
Aidas <linikas.aidas@gmail.com>
2019-11-27 00:25:21 +0200

    GameDataProviderTests implementation WIP

--- XUnitTest/Providers/GameData/GameDataProviderTests.cs
+++ XUnitTest/Providers/GameData/GameDataProviderTests.cs
@@ -11,7 +11,14 @@ namespace GameClient.Providers.GameData.Tests
         [Fact()]
         public void GameDataProviderTest()
         {
-            Assert.True(false, "This test needs an implementation");
+            GameClient.Providers.GameData.GameDataProvider provider1 = new GameClient.Providers.GameData.GameDataProvider();
+            GameClient.Observers.GameData.GameDataObserver observer1 = new GameClient.Observers.GameData.GameDataObserver();
+            List<IObserver<GameData>> observerList = new List<IObserver<GameData>>();
+            observerList.Add(observer1);
+        GameClient.Providers.GameData.GameDataUnsubscriber unsubscriber = new GameClient.Providers.GameData.GameDataUnsubscriber(observerList, observer1);
+            var temp = provider1.Subscribe(observer1);
+            Assert.Equal(unsubscriber, temp);
+            
         }
     }
 }
