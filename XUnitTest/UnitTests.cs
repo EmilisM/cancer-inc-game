cc7db80b5569ffa296b14ab517a0e8e6e7b8cb54
Aidas <linikas.aidas@gmail.com>
2019-11-26 19:39:13 +0200

    Unit testing init with couple of tests

---
+++ XUnitTest/UnitTests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using Xunit;
+using GameClient;
+using GameServer;
+
+namespace XUnitTest
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void InstanceCountTest()
+        {
+
+            GameServer.Entities.CancerIncGameBaseContext _instance = null;
+            GameServer.Entities.CancerIncGameBaseContext _instance2 = null;
+            var thread1 = (new Thread(delegate ()
+            {
+                _instance = GameServer.Entities.CancerIncGameBaseContext.GetInstance();
+            }));
+            var thread2 = (new Thread(delegate ()
+            {
+                _instance2 = GameServer.Entities.CancerIncGameBaseContext.GetInstance();
+            }));
+            thread1.Start();
+            thread2.Start();
+            thread1.Join();
+            thread2.Join();
+
+            Assert.Equal(_instance.ContextId, _instance2.ContextId);
+
+        }
+        [Fact]
+        public void GameConstantsTest()
+        {
+            double MainWindowHeight = 776;
+            double MainWindowWidth = 976;
+            double GameViewCanvasHeight = 576;
+            double LoggerRowDefinition = 200;
+            double LoggerHeight = 160;
+            Assert.Equal(MainWindowHeight, GameClient.Constants.GameConstants.MainWindowHeight);
+            Assert.Equal(MainWindowWidth, GameClient.Constants.GameConstants.MainWindowWidth);
+            Assert.Equal(GameViewCanvasHeight, GameClient.Constants.GameConstants.GameViewCanvasHeight);
+            Assert.Equal(LoggerRowDefinition, GameClient.Constants.GameConstants.LoggerRowDefinition);
+            Assert.Equal(LoggerHeight, GameClient.Constants.GameConstants.LoggerHeight);
+        }
+        [Fact]
+        public void GreenClassTest()
+        {
+            GameClient.GameObjects.Class.Factory.ClassFactory factory = new GameClient.GameObjects.Class.Factory.ClassFactory();
+            var temp = factory.GetClass(GameClient.GameObjects.Types.ClassType.Green);
+            Console.WriteLine(temp);
+        }
+    }
+}
